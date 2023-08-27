c6439d49bdbd1b2125cf3e74ac69d4558ba257ed
unknown <ojitdebnathfreelancer.com>
2023-08-27 12:15:07 +0600

    next page data fetching practice

---
+++ src/app/product/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-2xl">Loading...</h1>
+    </div>
+  );
+};
+
+export default loading;
